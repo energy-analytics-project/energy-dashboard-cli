4c919c36c5801eac1918004a95d5ee94d958530a
Todd Greenwood-Geer <pub+github@zwrob.com>
2019-09-16 16:00:07 -0700

    add GPL3 license snippet to main.py

--- edc/main.py
+++ edc/main.py
@@ -1,3 +1,19 @@
+# edc : Energy Dashboard Command Line Interface
+# Copyright (C) 2019  Todd Greenwood-Geer (Enviro Software Solutions, LLC)
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from edl.cli import feed as clifeed
 from edl.cli import feeds as clifeeds
 from edl.resources.exec import runyield
