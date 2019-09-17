813aed6c3f397ae8303e570f40984532df85e9bc
Todd Greenwood-Geer <pub+github@zwrob.com>
2019-09-17 07:28:36 -0700

    add 'save' to tests

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,5 @@
 EDDIR=/mnt/PASSPORT/data/eap/energy-dashboard
-LOGLEVEL=DEBUG
+LOGLEVEL=INFO
 PREFIX="--log-level ${LOGLEVEL}"
 TESTFEED="abc-test-01"
 #set -x
@@ -37,17 +37,20 @@ runcmd "edc ${PREFIX} feeds"
 runcmd "edc ${PREFIX} feeds list"
 runcmd "edc ${PREFIX} feed"
 # may run against already created feed for speed
-#runcmd_ignore_errors "edc ${PREFIX} feed ${TESTFEED} create -sdy 2019 -sdm 9 -sdd 1 --url=http://zwrob.com/assets/oasis_SZ_q_AS_MILEAGE_CALC_anc_type_ALL_sdt__START_T07_00-0000_edt__END_T07_00-0000_v_1.zip"
-runcmd "edc ${PREFIX} feed ${TESTFEED} create -sdy 2019 -sdm 9 -sdd 1 --url=http://zwrob.com/assets/oasis_SZ_q_AS_MILEAGE_CALC_anc_type_ALL_sdt__START_T07_00-0000_edt__END_T07_00-0000_v_1.zip"
+runcmd_ignore_errors "edc ${PREFIX} feed ${TESTFEED} create -sdy 2019 -sdm 9 -sdd 1 --url=http://zwrob.com/assets/oasis_SZ_q_AS_MILEAGE_CALC_anc_type_ALL_sdt__START_T07_00-0000_edt__END_T07_00-0000_v_1.zip"
+#runcmd "edc ${PREFIX} feed ${TESTFEED} create -sdy 2019 -sdm 9 -sdd 1 --url=http://zwrob.com/assets/oasis_SZ_q_AS_MILEAGE_CALC_anc_type_ALL_sdt__START_T07_00-0000_edt__END_T07_00-0000_v_1.zip"
 runcmd "edc ${PREFIX} feed ${TESTFEED} manifest show"
 runcmd "edc ${PREFIX} feed ${TESTFEED} invoke \"ls\""
 runcmd "edc ${PREFIX} feed ${TESTFEED} status --header"
 # if takes too long to download, comment out. TODO: need to fix test
-runcmd "edc ${PREFIX} feed ${TESTFEED} proc download"
+#runcmd "edc ${PREFIX} feed ${TESTFEED} proc download"
 runcmd "edc ${PREFIX} feed ${TESTFEED} proc unzip"
 runcmd "edc ${PREFIX} feed ${TESTFEED} proc parse"
 runcmd "edc ${PREFIX} feed ${TESTFEED} proc insert"
+runcmd_ignore_errors "edc ${PREFIX} feed ${TESTFEED} proc save"
 runcmd "edc ${PREFIX} feed ${TESTFEED} status --header"
 runcmd "edc ${PREFIX} feed ${TESTFEED} archive"
 #popd
 #runcmd "edc ${PREFIX} feed ${TESTFEED} reset parse"
+
+echo "TEST PASSED"
